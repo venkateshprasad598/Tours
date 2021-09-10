d950b856354d81720f8773f84cfb8e69bf52066a
venkateshprasad598 <venkateshprasad598@gmail.com>
2021-09-10 08:36:20 +0530

    Add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 import Axios from "axios";
 import Item from "./components/Item";
+import Loading from "./components/Loading";
 
 // const api = axios.create({
 //   baseURL: "https://course-api.com/react-tours-project",
@@ -22,39 +23,32 @@ function App() {
 
   const axios = async () => {
     const respnonse = await Axios.get(url);
-    setIsLoading(false);
+    console.log();
     setTours(respnonse.data);
+    console.log("Hello");
+    setIsLoading(false);
   };
 
   useEffect(() => {
     axios();
   }, []);
-  console.log(tours);
 
-  if (tours.length === 0) {
-    let refresh = (
-      <div>
-        <h2>No Tours Left</h2>
-        <button>Refresh</button>
+  if (isLoading) {
+    return <Loading />;
+  } else if (tours.length === 0) {
+    return (
+      <div className="App">
+        <h1>No Tours Left</h1>
+        <button onClick={() => axios()} className="refresh__button">
+          Refresh
+        </button>
       </div>
     );
   }
 
   return (
     <div className="App">
-      {isLoading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <Item tours={tours} click={notInterested} />
-      )}
-      {tours.length === 0 && isLoading === false ? (
-        <div>
-          <h1>No Tours Left</h1>
-          <button onClick={() => axios()} className="refresh__button">
-            Refresh
-          </button>
-        </div>
-      ) : null}
+      <Item tours={tours} click={notInterested} />
     </div>
   );
 }
